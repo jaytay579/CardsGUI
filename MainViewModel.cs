92a3cdab56bcdaf369e5f5a32650b4de2609552d
jaytay579 <jasontaylor579@gmail.com>
2024-02-24 02:21:42 +0000

    Add project files.

---
+++ MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CardsGUI
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<ImageItem> Images { get; set; }
+
+        public MainViewModel()
+        {
+            Images = new ObservableCollection<ImageItem> { // initalizing images
+            new ImageItem { ImagePath = "pack://application:,,,/images/card_clubs_02.png"},
+            new ImageItem { ImagePath = "pack://application:,,,/images/card_clubs_03.png"},
+            new ImageItem { ImagePath = "pack://application:,,,/images/card_clubs_04.png"},
+            };
+        }
+    }
+}
